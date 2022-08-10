aa0eb367e0806d692540d0b198cf5163327543e6
Rishabh Jain <hello@rishabhjain.dev>
2022-08-10 18:00:46 +0530

    fix: lontra social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,25 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className="flex justify-center py-2">
       <div className="flex items-center gap-x-2">
         <span className="flex items-center justify-center">
-          <figure>
-            <Image alt="linkedin" height={25} src="/images/linkedin.svg" width={30} />
-          </figure>
-          <figure>
-            <Image alt="twitter" height={25} src="/images/twitter.svg" width={30} />
-          </figure>
+          <Link href="https://www.linkedin.com/company/lontra-ventures">
+            <a>
+              <figure>
+                <Image alt="linkedin" height={25} src="/images/linkedin.svg" width={30} />
+              </figure>
+            </a>
+          </Link>
+          <Link href="https://twitter.com/lontraventures">
+            <a>
+              <figure>
+                <Image alt="twitter" height={25} src="/images/twitter.svg" width={30} />
+              </figure>
+            </a>
+          </Link>
         </span>
         <a href="https://www.lontraventures.com/">© {new Date().getFullYear()} by Lontra Ventures.</a>
       </div>
