1250c0b71787a9b8d32a122f4607b5da591a03c5
Rishabh Jain <hello@rishabhjain.dev>
2024-04-23 01:37:24 +0530

    feat: Update logos and crypto name

---
+++ config/portfolios.ts
@@ -0,0 +1,423 @@
+export type Company = {
+  className?: string;
+  name?: string;
+  logo: {
+    href: string;
+    url: string;
+    width: number;
+  }[];
+};
+
+export const Security: Company[] = [
+  {
+    logo: [
+      {
+        href: 'https://www.balkan.id/',
+        url: '/images/balkanId-logo.jpeg',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://ironcorelabs.com/',
+        url: '/images/IronCoreLabs-Logo.png',
+        width: 400,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://protopia.ai/',
+        url: '/images/Protopia-1024x512_tcm19-61758-_1_.webp',
+        width: 400,
+      },
+    ],
+  },
+
+  {
+    logo: [{ href: 'https://snyk.io/', url: '/images/snyk-logo.png', width: 100 }],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.symmetry-systems.com/',
+        url: '/images/symmetry-logo.png',
+        width: 200,
+      },
+    ],
+  },
+];
+
+export const Software: Company[] = [
+  {
+    logo: [
+      {
+        href: 'https://www.2048.vc/',
+        url: '/images/2048-ventures-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.airtory.com/',
+        url: '/images/airtory-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.molecula.com/',
+        url: '/images/Molecula-logo.png',
+        width: 200,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.onemodel.co/',
+        url: '/images/oneModel-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.reactivesearch.io/',
+        url: '/images/reactive-search-logo.png',
+        width: 250,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://getsift.ai/',
+        url: '/images/siftai_logo.jpg',
+        width: 200,
+      },
+    ],
+    name: 'Sift',
+  },
+  {
+    logo: [{ href: 'https://rackn.com/', url: '/images/RackN-Logo.png', width: 200 }],
+  },
+  {
+    logo: [
+      {
+        href: 'https://voltrondata.com/',
+        url: '/images/voltron-logo.svg',
+        width: 300,
+      },
+    ],
+  },
+];
+
+export const DigitalAssets: Company[] = [
+  {
+    logo: [
+      {
+        href: 'https://credmark.com/',
+        url: '/images/Credmark.jpeg',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.dcspark.io/',
+        url: '/images/dcSpark-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    className: 'bg-primary',
+    logo: [
+      {
+        href: 'https://karrier.one/',
+        url: '/images/karrierone-logo.png',
+        width: 250,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.milkomeda.com/',
+        url: '/images/milkomeda-logo.png',
+        width: 250,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://multicoin.capital/',
+        url: '/images/multi-coin-logo.jpeg',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://picante.io/',
+        url: '/images/picante-logo.png',
+        width: 200,
+      },
+    ],
+  },
+
+  {
+    logo: [{ href: 'https://quai.network/', url: '/images/quai.png', width: 250 }],
+  },
+  {
+    logo: [
+      {
+        href: 'https://ten31.vc/home',
+        url: '/images/ten31-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.truetribe.xyz/',
+        url: '/images/True Tribe Blue.png',
+        width: 250,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://unchained.com/',
+        url: '/images/UC-logo.png',
+        width: 200,
+      },
+    ],
+  },
+];
+
+export const Emerging: Company[] = [
+  {
+    logo: [
+      {
+        href: 'https://www.allencontrolsystems.com/',
+        url: '/images/allen_control_systems_logo.jpg',
+        width: 150,
+      },
+    ],
+    name: 'Allen Control Systems',
+  },
+  {
+    className: 'bg-primary',
+    logo: [
+      {
+        href: 'https://www.contoro.com/',
+        url: '/images/cropped-cropped-cropped-11-1-300x82.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.crowdout.com/',
+        url: '/images/crowdout_logo.png',
+        width: 90,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.dignifi.com/',
+        url: '/images/dignifi-logo.png',
+        width: 170,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://join5x5.com/',
+        url: '/images/five_five-logo.png',
+        width: 120,
+      },
+    ],
+  },
+  {
+    logo: [{ href: 'https://gxc.io/', url: '/images/gxc-logo.jpg', width: 150 }],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.orbitfab.com/',
+        url: '/images/orbitfab-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.shinkei.systems/',
+        url: '/images/shinkei-systems.jpeg',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.zenopower.com/',
+        url: '/images/zeno_power_systems_logo.jpg',
+        width: 150,
+      },
+    ],
+  },
+];
+
+export const Healthcare: Company[] = [
+  {
+    logo: [
+      {
+        href: 'https://docstation.co/',
+        url: '/images/docstation_logo.png',
+        width: 170,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://frontrow.co.in/',
+        url: '/images/frontrow-logo.png',
+        width: 200,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://joshin.com/',
+        url: '/images/joshin-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://ostiio.com/',
+        url: '/images/Ostiio_logo.png',
+        width: 200,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.subjectwell.com/',
+        url: '/images/subjectwell-logo.png',
+        width: 200,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.vitalflohealth.com/',
+        url: '/images/vital-flo-logo.png',
+        width: 150,
+      },
+    ],
+  },
+];
+
+export const Exits: Company[] = [
+  {
+    logo: [
+      {
+        href: 'https://www.onepeloton.com/',
+        url: '/images/atlas-logo.png',
+        width: 150,
+      },
+      {
+        href: 'https://www.onepeloton.com/',
+        url: '/images/peloton-logo.jpeg',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://stoplight.io/',
+        url: '/images/Smartbear.png',
+        width: 200,
+      },
+      {
+        href: 'https://stoplight.io/',
+        url: '/images/stoplight.png',
+        width: 200,
+      },
+    ],
+  },
+
+  {
+    logo: [
+      {
+        href: 'https://www.vmware.com/',
+        url: '/images/bitfusion-logo.png',
+        width: 150,
+      },
+      {
+        href: 'https://www.vmware.com/',
+        url: '/images/VMWare-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  {
+    logo: [
+      {
+        href: 'https://www.optimizely.com/',
+        url: '/images/experimentengine-logo.png',
+        width: 250,
+      },
+      {
+        href: 'https://www.optimizely.com/',
+        url: '/images/Optimizely-logo.png',
+        width: 150,
+      },
+    ],
+  },
+  // { logo: [] },
+  {
+    logo: [
+      {
+        href: 'https://www.mediafly.com/',
+        url: '/images/olono_logo.png',
+        width: 170,
+      },
+      {
+        href: 'https://www.mediafly.com/',
+        url: '/images/MediaFly-logo.jpeg',
+        width: 200,
+      },
+    ],
+  },
+];
